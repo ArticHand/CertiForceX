c07d8bcaf2b1c1e635073fab3b476e9e1a20ef31
richardla <thisisrichardla@gmail.com>
2025-05-28 16:49:49 +0800

    fix unit test and build pipeline

--- CertUtils.Test/UnitTest1.cs
+++ CertUtils.Test/UnitTest1.cs
@@ -1,59 +1,73 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security.Cryptography;
+using System.Threading.Tasks;
 using CertUtils.Cert;
 using CertUtils.Salesforce;
 using CertUtils.Token;
+using Xunit;
 
 namespace CertUtils.Test;
 
-public class UnitTest1
+public class UnitTest1 : IDisposable
 {
+    private readonly RSA _rsa;
+    private readonly IPrivateKeyProvider _privateKeyProvider;
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly ITokenRequester _tokenRequester;
+    private readonly SalesforceTokenProvider _tokenProvider;
+
+    private const string ClientId = "3MVG9GCMQoQ6rpzR0ZxpRnvsA_pWCNS2PpUUNcJ4ujokj80WCNo7EBGx9pgRbzhqmUAuQM6_kJsMzQCEmf_sJ";
+    private const string Username = "richardla@vandewiele.test";
+    private const string AuthEndpoint = "https://vandewiele-dev-ed.develop.my.salesforce.com";
+    private const string Aud = "https://test.salesforce.com";
+    private const string PrivateKeyUrl = "https://gist.githubusercontent.com/ArticHand/e6a938c34e7102f2a9d72080242143e4/raw/a946ba4303a2813b0b4369bcba32dba81f1f41e2/gistfile1.txt";
+
+    public UnitTest1()
+    {
+        // Initialize RSA with a new key for each test
+        _rsa = RSA.Create(2048);
+        _privateKeyProvider = new UrlPrivateKeyProvider(PrivateKeyUrl);
+        _jwtTokenGenerator = new JwtTokenGenerator(ClientId, Username, Aud, AuthEndpoint);
+        _tokenRequester = new SalesforceTokenRequester(AuthEndpoint);
+        _tokenProvider = new SalesforceTokenProvider(_privateKeyProvider, _jwtTokenGenerator, _tokenRequester);
+    }
+
     [Fact]
     public void GetPrivateKeyReturn_True()
     {
-        var privateKeyUrl = "https://pastebin.com/raw/pdykvseW";
-        // Dependency Injection
-        IPrivateKeyProvider privateKeyProvider = new UrlPrivateKeyProvider(privateKeyUrl);
-        var pkey=privateKeyProvider.GetPrivateKeyAsync().Result;
+        // Act
+        var pkey = _privateKeyProvider.GetPrivateKeyAsync().Result;
+        
+        // Assert
+        Assert.NotNull(pkey);
         Assert.NotEmpty(pkey);
-
     }
 
     [Fact]
     public void GetJwtString_True()
     {
-        var clientId = "3MVG9GCMQoQ6rpzR0ZxpRnvsA_pWCNS2PpUUNcJ4ujokj80WCNo7EBGx9pgRbzhqmUAuQM6_kJsMzQCEmf_sJ";
-        var username = "richardla@vandewiele.test";
-        var authEndpoint = "https://vandewiele-dev-ed.develop.my.salesforce.com";
-        var aud = "https://test.salesforce.com";
-        var privateKeyUrl = "https://gist.githubusercontent.com/ArticHand/e6a938c34e7102f2a9d72080242143e4/raw/a946ba4303a2813b0b4369bcba32dba81f1f41e2/gistfile1.txt";
-        // Dependency Injection
+        // Arrange
+        var pkey = _privateKeyProvider.GetPrivateKeyAsync().Result;
 
-        IPrivateKeyProvider privateKeyProvider = new UrlPrivateKeyProvider(privateKeyUrl);
-        var pkey=privateKeyProvider.GetPrivateKeyAsync().Result;
-
-        // Dependency Injection
-        IJwtTokenGenerator jwtTokenGenerator = new JwtTokenGenerator(clientId, username,aud, authEndpoint);
-        var jwt=jwtTokenGenerator.GenerateJwtToken(pkey);
+        // Act
+        var jwt = _jwtTokenGenerator.GenerateJwtToken(pkey);
+        
+        // Assert
+        Assert.NotNull(jwt);
         Assert.NotEmpty(jwt);
     }
 
-        [Fact]
+    [Fact]
     public async Task GetSalesforceToken_True()
     {
-        var clientId = "3MVG9GCMQoQ6rpzR0ZxpRnvsA_pWCNS2PpUUNcJ4ujokj80WCNo7EBGx9pgRbzhqmUAuQM6_kJsMzQCEmf_sJ";
-        var username = "richardla@vandewiele.test";
-        var authEndpoint = "https://vandewiele-dev-ed.develop.my.salesforce.com/services/oauth2/token";
-        var aud = "https://test.salesforce.com";
-        var privateKeyUrl = "https://gist.githubusercontent.com/ArticHand/e6a938c34e7102f2a9d72080242143e4/raw/a946ba4303a2813b0b4369bcba32dba81f1f41e2/gistfile1.txt";
-        // Dependency Injection
-        IPrivateKeyProvider privateKeyProvider = new UrlPrivateKeyProvider(privateKeyUrl);
-        // Dependency Injection
-        IJwtTokenGenerator jwtTokenGenerator = new JwtTokenGenerator(clientId, username,aud, authEndpoint);
-        ITokenRequester requester=new SalesforceTokenRequester(authEndpoint);
-        SalesforceTokenProvider provider =new SalesforceTokenProvider(privateKeyProvider,jwtTokenGenerator,requester);
-        var sfToken=await provider.GetAccessTokenAsync();
-        Assert.NotNull(sfToken);
+        // Act
+        var sfToken = await _tokenProvider.GetAccessTokenAsync();
         
+        // Assert
+        Assert.NotNull(sfToken);
+    }
+
+    public void Dispose()
+    {
+        _rsa?.Dispose();
     }
 }
-
